18b89fbee1c403dde7e5903ceda20b7ea5af334f
liyang <ly18846162402@163.com>
2024-01-22 19:18:55 +0800

    chore: update gtctl version v0.1.0-beta.9

--- Formula/gtctl.rb
+++ Formula/gtctl.rb
@@ -1,14 +1,14 @@
 class Gtctl < Formula
   desc "gtctl is a command-line tool for managing GreptimeDB cluster."
   homepage "https://github.com/GreptimeTeam/gtctl"
-  version "v0.1.0-beta.8"
+  version "v0.1.0-beta.9"
 
   if Hardware::CPU.intel?
-    url "https://github.com/GreptimeTeam/gtctl/releases/download/v0.1.0-beta.8/gtctl-darwin-amd64.tgz"
-    sha256 "73d478d0a7a2d1db4c71220593f45bb996f3ac497a8e00af003d11d79de6ce11"
+    url "https://github.com/GreptimeTeam/gtctl/releases/download/v0.1.0-beta.9/gtctl-darwin-amd64.tgz"
+    sha256 "19511c242b0a9f028227247439c261fd58bea1e0abe0ea62047d0248eb1bec24"
   elsif Hardware::CPU.arm?
-    url "https://github.com/GreptimeTeam/gtctl/releases/download/v0.1.0-beta.8/gtctl-darwin-arm64.tgz"
-    sha256 "0819733989a0a145229cd638f13989053ad55c8eb35b15a15e2175bcdd5259b9"
+    url "https://github.com/GreptimeTeam/gtctl/releases/download/v0.1.0-beta.9/gtctl-darwin-arm64.tgz"
+    sha256 "db78316edd330c187e5478a4db06db82e54c2353112d38dd2a91853c604d5a61"
   end
 
   def install
