d329d9fce4d2edb7e42b7e0291970044954d47d9
tison <wander4096@gmail.com>
2024-03-22 18:42:17 +0800

    feat: integrate with brew services start/stop (#7)
    Signed-off-by: tison <wander4096@gmail.com>

--- Formula/greptime.rb
+++ Formula/greptime.rb
@@ -2,6 +2,7 @@ class Greptime < Formula
   desc "An open-source, cloud-native, distributed time-series database with PromQL/SQL/Python supported."
   homepage "https://github.com/GreptimeTeam/greptimedb"
   version "v0.7.1"
+  license "Apache-2.0"
 
   if Hardware::CPU.intel?
     url "https://github.com/GreptimeTeam/greptimedb/releases/download/v0.7.1/greptime-darwin-amd64-v0.7.1.tar.gz"
@@ -14,5 +15,12 @@ class Greptime < Formula
   def install
     bin.install "greptime"
   end
-end
 
+  service do
+    run [opt_bin/"greptime", "standalone", "start"]
+    keep_alive true
+    working_dir HOMEBREW_PREFIX
+    log_path var/"log/greptimedb/greptime_output.log"
+    error_log_path var/"log/greptimedb/greptime_output.log"
+  end
+end
