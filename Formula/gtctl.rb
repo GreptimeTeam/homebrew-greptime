a7d33f617a1aa7aa1092c4f7f865dcd7d06f5f37
liyang <ly18846162402@163.com>
2024-08-13 11:31:50 +0800

    chore: bump gtctl version from v0.1.0-beta.9 to v0.1.0-beta.11

--- Formula/gtctl.rb
+++ Formula/gtctl.rb
@@ -1,14 +1,14 @@
 class Gtctl < Formula
   desc "gtctl is a command-line tool for managing GreptimeDB cluster."
   homepage "https://github.com/GreptimeTeam/gtctl"
-  version "v0.1.0-beta.9"
+  version "v0.1.0-beta.11"
 
   if Hardware::CPU.intel?
-    url "https://github.com/GreptimeTeam/gtctl/releases/download/v0.1.0-beta.9/gtctl-darwin-amd64.tgz"
-    sha256 "19511c242b0a9f028227247439c261fd58bea1e0abe0ea62047d0248eb1bec24"
+    url "https://github.com/GreptimeTeam/gtctl/releases/download/v0.1.0-beta.11/gtctl-darwin-amd64.tgz"
+    sha256 "c8d61b8ee32e42697624bf40e0e63b8fb49aa6b00030751d6c1f922d0ccd003e"
   elsif Hardware::CPU.arm?
-    url "https://github.com/GreptimeTeam/gtctl/releases/download/v0.1.0-beta.9/gtctl-darwin-arm64.tgz"
-    sha256 "db78316edd330c187e5478a4db06db82e54c2353112d38dd2a91853c604d5a61"
+    url "https://github.com/GreptimeTeam/gtctl/releases/download/v0.1.0-beta.11/gtctl-darwin-arm64.tgz"
+    sha256 "3dd81cb8f0e05cded36e30add114bd02146c743127178471561ba0e5bab8b067"
   end
 
   def install
