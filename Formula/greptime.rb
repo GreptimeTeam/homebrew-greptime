3e263c02105459fbc93ec2e5d34ec34ce439d135
liyang <ly18846162402@163.com>
2024-10-16 15:46:08 +0800

    chore: bump greptimedb version from v0.9.3 to v0.9.4 (#17)

--- Formula/greptime.rb
+++ Formula/greptime.rb
@@ -1,15 +1,15 @@
 class Greptime < Formula
   desc "An open-source, cloud-native, distributed time-series database with PromQL/SQL/Python supported."
   homepage "https://github.com/GreptimeTeam/greptimedb"
-  version "v0.9.3"
+  version "v0.9.4"
   license "Apache-2.0"
 
   if Hardware::CPU.intel?
-    url "https://github.com/GreptimeTeam/greptimedb/releases/download/v0.9.3/greptime-darwin-amd64-v0.9.3.tar.gz"
-    sha256 "ef2d151624f4c56906468197e5130a8050b7442b72b32925d8968969de84a5db"
+    url "https://github.com/GreptimeTeam/greptimedb/releases/download/v0.9.4/greptime-darwin-amd64-v0.9.4.tar.gz"
+    sha256 "d1c48df56c9f9f886f643d794252b9827318fd6368a6141ae102fa4863225d69"
   elsif Hardware::CPU.arm?
-    url "https://github.com/GreptimeTeam/greptimedb/releases/download/v0.9.3/greptime-darwin-arm64-v0.9.3.tar.gz"
-    sha256 "cee1f3bb1c61b965dc5ce9c72622f6b2aea78c3e4a84d693ad1b036819f8d61e"
+    url "https://github.com/GreptimeTeam/greptimedb/releases/download/v0.9.4/greptime-darwin-arm64-v0.9.4.tar.gz"
+    sha256 "2b901773631d65f2d2de5bd8c9898160fc5eefddeef27bb595a8abd3cad2b8bc"
   end
 
   def install
