d38894e8891e236c5a4098a879ea53f700303216
liyang <daviderli614@gmail.com>
2026-06-15 10:51:08 +0800

    chore: Update GreptimeDB version to v1.1.0 (#64)
    Signed-off-by: update-greptime-version <update-greptime-version@greptime.com>
    Co-authored-by: update-greptime-version <update-greptime-version@greptime.com>

--- Formula/greptime.rb
+++ Formula/greptime.rb
@@ -1,15 +1,15 @@
 class Greptime < Formula
   desc "An open-source, cloud-native, distributed time-series database with PromQL/SQL/Python supported."
   homepage "https://github.com/GreptimeTeam/greptimedb"
-  version "v1.0.2"
+  version "v1.1.0"
   license "Apache-2.0"
 
   if Hardware::CPU.intel?
-    url "https://github.com/GreptimeTeam/greptimedb/releases/download/v1.0.2/greptime-darwin-amd64-v1.0.2.tar.gz"
-    sha256 "e6567f5d97fd34b876b5653c22877c238a1633ecbe26b3b7c22b95892656c39c"
+    url "https://github.com/GreptimeTeam/greptimedb/releases/download/v1.1.0/greptime-darwin-amd64-v1.1.0.tar.gz"
+    sha256 "c4459d6f4c2c2d4420d0da8e0edb0589326f6e1a2cb876fbf3a2612c4c4c6ee7"
   elsif Hardware::CPU.arm?
-    url "https://github.com/GreptimeTeam/greptimedb/releases/download/v1.0.2/greptime-darwin-arm64-v1.0.2.tar.gz"
-    sha256 "ba460857f96c90889a23633e792fafda7f33979e44ce5e82822cd56653b6008c"
+    url "https://github.com/GreptimeTeam/greptimedb/releases/download/v1.1.0/greptime-darwin-arm64-v1.1.0.tar.gz"
+    sha256 "35f5c4c7e23a9cbfb48c71a49d0fd4c54fbb06ca9f89326a4eabbf5c47b5b481"
   end
 
   def install
